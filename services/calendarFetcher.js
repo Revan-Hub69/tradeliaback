770de33e78c898ae63c7681b9ced43b32341b3ea
Revan-Hub69 <topofferte@protonmail.ch>
2025-04-20 03:27:02 +0200

    Create calendarFetcher.js

---
+++ services/calendarFetcher.js
@@ -0,0 +1,18 @@
+{
+  "macro": [
+    {
+      "country": "US",
+      "event": "CPI",
+      "impact": "High",
+      ...
+    }
+  ],
+  "crypto": [
+    {
+      "coin": "Ethereum",
+      "event": "Dencun Hard Fork",
+      "date": "2025-04-21",
+      "proof": "https://link.com/event"
+    }
+  ]
+}
